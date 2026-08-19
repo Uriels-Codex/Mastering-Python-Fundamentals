06d7c37fe54e20a369028e15039122dabbff05b8
UrieL <208273914+Uriels-Codex@users.noreply.github.com>
2026-08-19 23:54:24 +0800

    Day 7: tuples & sets - immutability, dict-key hashing, count() nesting bug, set operations

--- day7_fundamentals.py
+++ day7_fundamentals.py
@@ -13,13 +13,26 @@
 # try using .count() first, see what happens, and be ready to explain why it does or 
 # doesn't work the way you expect.
 
-students = [("Uriel", 1), ("Jamir", "BSIT"), ("David", "BSIT")]
-for name, course in students:
-    print(f"Name:{name}, course is: {course}")
-print(f"Total count of BSCS course: {students.count("BSIT")}")
-print(students.count(1))
+# students = [("Uriel", 1), ("Jamir", "BSIT"), ("David", "BSIT")]
+# for name, course in students:
+#     print(f"Name:{name}, course is: {course}")
+# print(f"Total count of BSCS course: {students.count("BSIT")}")
+# print(students.count(1))
 
-# numbers = (23, 23, 23, 23,12 ,235,12,4,5,1)
-numbers = [(23, 23), (23, 23), (12 ,235),(12,4),(5,1)]
+# # numbers = (23, 23, 23, 23,12 ,235,12,4,5,1) dxc
 
-print(numbers.count(23))
+# numbers = [(23, 23), (23, 23), (12 ,235),(12,4),(5,1)]
+
+# print(numbers.count(23))
+
+
+# sets - built in data typem usd to store unoredered collection
+# class_a = {"Uriel", "Brent", "Jamir", "David"}
+# class_b = {"Jamir", "Aki", "Ariel"}
+
+# print(f"this is union: {class_a | class_b}")
+# print(f"this is intersection: {class_a & class_b}")
+# print(f"this is difference: {class_a - class_b}")
+# print(f"this is systemetric difference: {class_a ^ class_b}")
+# class_a.add("Kyle")
+# print(f"Does Kyle exist in clas_a? {"Kyle" in class_a}")
